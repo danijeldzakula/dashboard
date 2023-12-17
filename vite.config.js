da70bf4c2367602b09ff9e68e2109ddaa2b25a86
danijeldzakula <danidzaki060@gmail.com>
2023-12-18 00:10:51 +0100

    update: router system navigation v1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import path, { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,6 +10,11 @@ export default defineConfig({
   build: {
     target: 'esnext',
     outDir: 'dist',
+  },
+  rollupOptions: {
+    input: {
+      main: resolve(__dirname, 'index.html'),
+    },
   },
   resolve: {
     alias: {
